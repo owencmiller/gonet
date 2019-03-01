6233ebe609a41eda244d4036f1418f76b0b4072c
Owen C. Miller <owenchristianmiller@gmail.com>
2019-03-01 12:56:10 -0500

    Export Matrix fields; Add copy matrix function; add biases to network

--- matrixutil/matrix.go
+++ matrixutil/matrix.go
@@ -6,80 +6,90 @@ import (
 
 // Matrix Structure
 type Matrix struct{
-	rows int
-	cols int
-	mat[][] float64
+	Rows int
+	Cols int
+	Mat[][] float64
 }
 
 // Method for multiplying two matrices
-func (mat1 Matrix) Multiply(mat2 Matrix) Matrix{
-	if mat1.cols != mat2.rows{
-		panic("matrix multiplication not possible")
+func (Mat1 Matrix) Multiply(Mat2 Matrix) Matrix{
+	if Mat1.Cols != Mat2.Rows{
+		panic("rix multiplication not possible")
 	}
-	ans := make([][]float64, mat1.rows)
+	ans := make([][]float64, Mat1.Rows)
 	for i, _ := range ans{
-		ans[i] = make([]float64, mat2.cols)
+		ans[i] = make([]float64, Mat2.Cols)
 		for j, _ := range ans[i]{
-			for k, _ := range mat2.mat{
-				ans[i][j] += mat1.mat[i][k]*mat2.mat[k][j]
+			for k, _ := range Mat2.Mat{
+				ans[i][j] += Mat1.Mat[i][k]*Mat2.Mat[k][j]
 			}
 		}
 	}
-	return Matrix{rows:len(mat1.mat), cols:len(mat2.mat[0]), mat: ans}
+	return Matrix{Rows:len(Mat1.Mat), Cols:len(Mat2.Mat[0]), Mat: ans}
 }
 
 // Apply a function to two matrices. (ex. subtraction, addition)
-func ApplyFunc(mat1 Matrix, mat2 Matrix, operation func(num1 float64,num2 float64) float64) Matrix{
-	if mat1.rows != mat2.rows || mat1.cols != mat2.cols{
-		panic("Can't apply function to different sized matrices")
+func ApplyFunc(Mat1 Matrix, Mat2 Matrix, operation func(num1 float64,num2 float64) float64) Matrix{
+	if Mat1.Rows != Mat2.Rows || Mat1.Cols != Mat2.Cols{
+		panic("Can't apply function to different sized rices")
 	}
-	for i, _ := range mat1.mat{
-		for j, _ := range mat1.mat[i]{
-			mat1.mat[i][j] = operation(mat1.mat[i][j], mat2.mat[i][j])
+	for i, _ := range Mat1.Mat{
+		for j, _ := range Mat1.Mat[i]{
+			Mat1.Mat[i][j] = operation(Mat1.Mat[i][j], Mat2.Mat[i][j])
 		}
 	}
-	return mat1
+	return Mat1
 }
 
-// Method for applying an operation to all elements in a matrix
+// Method for applying an operation to all elements in a rix
 func (matrix Matrix) ApplyConst(operation func(num float64) float64){
-	for i := range matrix.mat{
-		for j := range matrix.mat[0]{
-			matrix.mat[i][j] = operation(matrix.mat[i][j])
+	for i := range matrix.Mat{
+		for j := range matrix.Mat[0]{
+			matrix.Mat[i][j] = operation(matrix.Mat[i][j])
 		}
 	}
 }
 
-// Create a Matrix from 2d array/slice
+// Create a matrix from 2d array/slice
 func CreateMatrix(data [][]float64) Matrix{
-	rows := len(data)
-	cols := len(data[0])
+	Rows := len(data)
+	Cols := len(data[0])
 
-	return Matrix{rows: rows, cols: cols, mat: data}
+	return Matrix{Rows: Rows, Cols: Cols, Mat: data}
 }
 
-// Generate a Matrix of zeros
-func GenerateMatrixZeros(rows, cols int) Matrix{
-	w := make([][]float64, rows)
+// Copy Matrix
+func CopyMatrix(mat Matrix) Matrix{
+	temp := make([][]float64, mat.Rows)
+	for i, _ := range temp{
+		temp[i] = make([]float64, mat.Cols)
+		for j, _ := range temp[i]{
+			temp[i][j] = mat.Mat[i][j]
+		}
+	}
+	return CreateMatrix(temp)
+}
+
+// Generate a rix of zeros
+func GenerateMatrixZeros(Rows, Cols int) Matrix{
+	w := make([][]float64, Rows)
 	for i, _ := range w {
-		w[i] = make([]float64, cols)
+		w[i] = make([]float64, Cols)
 		for j, _ := range w[i]{
 			w[i][j] = 0
 		}
 	}
-	mat := Matrix{rows: rows, cols: cols, mat: w}
-	return mat
+	return Matrix{Rows: Rows, Cols: Cols, Mat: w}
 }
 
-// Generate a matrix of random float64's
-func GenerateMatrixRand(rows, cols int) Matrix{
-	w := make([][]float64, rows)
-	for i, _ := range w {
-		w[i] = make([]float64, cols)
+// Generate a rix of random float64's
+func GenerateMatrixRand(Rows, Cols int) Matrix{
+	w := make([][]float64, Rows)
+	for i, _ := range w{ 
+		w[i] = make([]float64, Cols)
 		for j, _ := range w[i]{
 			w[i][j] = rand.Float64()
 		}
 	}
-	mat := Matrix{rows: rows, cols: cols, mat: w}
-	return mat
+	return Matrix{Rows: Rows, Cols: Cols, Mat: w}
 }
--- net/net.go
+++ net/net.go
@@ -9,14 +9,29 @@ import (
 // Neural Network layers of weights
 type Network struct{
 	layers[] mu.Matrix
-	//TODO add biases
+	bias[] mu.Matrix
 }
 
-
-// Forward propogate through the Network TODO make this a method for the Network
+func addBias(weights mu.Matrix, bias mu.Matrix) mu.Matrix{
+	if weights.Cols != bias.Cols{
+		panic("Bias cannot be added")
+	}
+	for i, row := range weights.Mat{
+		temp := make([][]float64, 1)
+		for _, val := range row{
+			temp[0] = append(temp[0], val)
+		}
+		weights.Mat[i] = mu.ApplyFunc(mu.CreateMatrix(temp), bias, func(num1 float64, num2 float64)float64{
+			return num1+num2
+		}).Mat[0]
+	}
+	return weights
+}
+// Forward propogate through the Network TODO make this a method for the Network (maybe)
 func forwardProp(input mu.Matrix, net Network) mu.Matrix{
-	for _, layer := range net.layers{
+	for i, layer := range net.layers{
 		input = input.Multiply(layer)
+		input = addBias(input, net.bias[i])
 		input.ApplyConst(sigmoid)
 	}
 	return input
@@ -28,6 +43,7 @@ func forwardProp(input mu.Matrix, net Network) mu.Matrix{
 
 // MSE Loss function
 func meanSquaredError(guess mu.Matrix, goal mu.Matrix) mu.Matrix{
+
 	diff := mu.ApplyFunc(guess, goal, func(num1 float64, num2 float64)float64{
 		return (math.Pow(num1-num2,2))/2
 	})
@@ -40,33 +56,32 @@ func sigmoid(num float64) float64{
 }
 
 // Create a NN from layerAmounts - including input and output amounts
-// TODO add bias creation
 func createNetwork(layerAmounts ...int) Network{
-	temp := make([]mu.Matrix, 0)
-	for i, num := range layerAmounts{
+	layers := make([]mu.Matrix, 0)
+	for i, val := range layerAmounts{
 		if i+1 == len(layerAmounts){
 			break
 		}
-		temp = append(temp, mu.GenerateMatrixRand(num, layerAmounts[i+1]))
+		layers = append(layers, mu.GenerateMatrixRand(val, layerAmounts[i+1]))
 	}
-	return Network{layers: temp}
+
+	biases := make([]mu.Matrix, 0)
+	for i, _ := range layerAmounts{
+		if i+1 == len(layerAmounts){ 
+			break 
+		}
+		biases = append(biases, mu.GenerateMatrixRand(1,layerAmounts[i+1]))
+	}
+	return Network{layers: layers, bias: biases}
 }
 
 // Testing and Running
 func main() {
+
 	input := [][]float64{
 		{1,1},
 		{0,0},
 	}
-	input2 := [][]float64{
-		{1,2},
-		{3,4},
-	}
-	
-	fmt.Println(mu.ApplyFunc(mu.CreateMatrix(input), mu.CreateMatrix(input2), func(num1 float64, num2 float64)float64{
-		return num1-num2
-	}))
-
 
 	goal := [][]float64{
 		{1},
@@ -74,9 +89,11 @@ func main() {
 	}
 	goalMat := mu.CreateMatrix(goal)
 
+
 	network := createNetwork(2,3,1)
 	output := forwardProp(mu.CreateMatrix(input), network)
-	loss := meanSquaredError(output, goalMat)
+	loss := meanSquaredError(mu.CopyMatrix(output), goalMat)
+
 	fmt.Println("Network:", network)
 	fmt.Println("Output:", output)
 	fmt.Println("Loss:", loss)
